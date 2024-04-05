0884bada0a0c492568a3fb50e65aa79a3925d802
RaelMartinss <raelpgm2@gmail.com>
2024-04-05 10:16:57 -0300

    ci: refact

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import "net/http"
 
 func main() {
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request)  {
-		w.Write([]byte("<h1>Hello Full Cycle!!!!!</h1>"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<h1>Hello Full Cycle</h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }

2de4b0c4c1655b5978b4d650da2ba9052fcefd93
RaelMartinss <raelpgm2@gmail.com>
2024-04-05 13:41:57 -0300

    feat:

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello Argo!!!!</h1>"))
+		w.Write([]byte("<h1>Hello Argo!!!!</h1> <p>tudo certo</p>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
